d6990a190e4f96c7d7efaa26b613cea98108c3aa
Karthikk7293 <karthikk7293@gmail.com>
2025-05-20 13:47:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import About from "./About";
+import style from "./app.module.css"
+
+function App() {
+
+  return (
+    <div className={style.logo}>
+      <About />
+      <h1>hello welcome!</h1>
+    </div>
+  )
+
+}
+
+export default App;
